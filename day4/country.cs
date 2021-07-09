d2bc10aeac3434144ce27bdbf2526617bc0d0165
veera-chitluri <85030841+veera-chitluri@users.noreply.github.com>
2021-07-09 07:26:14 +0530

    Add files via upload

---
+++ day4/country.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace country
+{
+    class Program
+    {
+        private static string india;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter country name:");
+            var country = Console.ReadLine();
+
+            Console.WriteLine($"Enter state name:");
+            var state = Console.ReadLine();
+            if (country == "india")
+            {
+              if (state == "kerala")
+            }
+             Console.WriteLine(true);
+             else {
+                 Console.WriteLine(false); 
+             }
+
+
+        }
+    }
+}
---
+++ day4/duplicate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace duplicate
+{
+    class Program
+    {
+     public  static void ArrayList()
+        {
+            Console.WriteLine("Enter the length of array");
+            int ArrayLength = Convert.ToInt32(Console.ReadLine());
+            var myArrayList = new ArrayList();
+            Console.WriteLine("Enter the elements of the array:");
+            var ArrayList = Convert.ToInt32(Console.ReadLine());
+            myArrayList.Add(44);
+             myArrayList.Add(75);
+           Console.WriteLine("Duplicate elements in given Array");
+           for(int i=0;  i<ArrayLength;  i++)
+           {
+                for(int j=0;  j<ArrayLength;  j++)
+                {
+                    if(myArrayList[i] == myArrayList[j])
+                    Console.WriteLine(myArrayList[j]);
+                }
+           }
+
+
+
+
+        
+             
+
+        
+            
+
+
+        }
+    }
+}
